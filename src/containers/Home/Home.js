fd72c826eb6cd980f42ea24c89917929c5c86df9
qinxl <39668590+qinxl@users.noreply.github.com>
2020-04-07 18:30:32 +0800

    登录页面

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { createHashHistory } from 'history';
+import { Button } from 'antd';
 
 function WorkBench() {
+    const history = createHashHistory();
+    const quitHandler = () => {
+        localStorage.clear();
+        history.push("/login");
+    };
+    const intoAdmin = () => {
+        history.push("/admin");
+    };
     return (
         <div>
             <h1>主页</h1>
+            <Button type="primary" onClick={() => intoAdmin()}>进入后台</Button>
+            <Button type="danger" onClick={() => quitHandler()}>退出登录</Button>
         </div>
     );
 }
--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import { createHashHistory } from 'history';
+import { Form, Input, Button, Card } from 'antd';
+import './style.css';
+
+const userNameRules = [
+    { required: true, message: '请输入用户名' },
+    { whitespace: true, message: '请输入用户名' },
+];
+const pwdRules = [
+    { required: true, message: '请输入密码' },
+    { whitespace: true, message: '请输入用户名' },
+];
 
 function Login() {
+    const history = createHashHistory();
+    const onFinish = values => {
+        console.log('Success:', values);
+        localStorage.setItem('token', new Date().getMilliseconds);
+        history.push("/home");
+    };
+
+    const onFinishFailed = errorInfo => {
+        console.log('Failed:', errorInfo);
+    };
+
     return (
-        <div>
-            <h1>登录页</h1>
-        </div>
+        <Card title="宁波工程学院教学辅助系统" className="main-container">
+            <Form labelCol={{ span: 5 }} wrapperCol={{ span: 19 }} name="basic" initialValues={{ remember: true }} onFinish={onFinish} onFinishFailed={onFinishFailed} >
+                <Form.Item label="用户名" name="username" rules={userNameRules} >
+                    <Input autoComplete="off" />
+                </Form.Item>
+                <Form.Item label="密码" name="password" rules={pwdRules} >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item wrapperCol={{ offset: 5, span: 19 }} >
+                    <Button type="primary" htmlType="submit" block>登录</Button>
+                </Form.Item>
+            </Form>
+        </Card>
     );
 }
-
 export default Login;
